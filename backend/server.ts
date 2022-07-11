c520381a28a54afa39711522170902fd42187d01
Tim-Pelykh <timpelykh@gmail.com>
2022-07-11 16:42:18 +0100

    Some minors changes for comfortable work

--- backend/server.ts
+++ backend/server.ts
@@ -6,6 +6,9 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
   res.send('Hello Earth');
 });
