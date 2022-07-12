15c39716ad7c881717b194581b620169faad0ef8
Tim-Pelykh <timpelykh@gmail.com>
2022-07-12 22:15:29 +0100

    Updated version of starting screen

--- frontend/src/screens/homeScreen/HomeScreen.tsx
+++ frontend/src/screens/homeScreen/HomeScreen.tsx
@@ -2,20 +2,28 @@ import './homeScreen.css';
 import { homeBG } from '../../images/images.js';
 import { arrowDown, circle } from '../../icons/icons.js';
 
+const hours = new Date().getHours();
+
 function HomeScreen() {
   return (
     <div className="pf__home">
       <div className="pf__home-start">
         <img src={homeBG} id="main-img" alt="Starting page" />
         <div className="pf__home-start_imgOverlay" />
-        <h1>Bonjour</h1>
-        <h2>Dear Guest</h2>
-        <img
-          src={arrowDown}
-          alt="Arrow goes down"
-          className="pf__home-start_arrow"
-        />
-        <img src={circle} alt="Circle" className="pf__home-start_circle" />
+        <div className="pf__home-start_content">
+          {hours >= 12 ? <h1>Bonsoir</h1> : <h1>Bonjoir</h1>}
+          <p>Dear Guest</p>
+          <img
+            src={arrowDown}
+            alt="Arrow goes down"
+            className="pf__home-start_content-arrow"
+          />
+          <img
+            src={circle}
+            alt="Circle"
+            className="pf__home-start_content-circle"
+          />
+        </div>
       </div>
     </div>
   );
