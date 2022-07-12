93b70e1e5ae668f3351c644980defea2474d7b3f
Tim-Pelykh <timpelykh@gmail.com>
2022-07-12 20:34:05 +0100

    Initial markup for Starting page was created

---
+++ frontend/src/icons/icons.js
@@ -0,0 +1,2 @@
+export { default as arrowDown } from './arrow-down.png';
+export { default as circle } from './circle.png';
--- frontend/src/screens/homeScreen/HomeScreen.tsx
+++ frontend/src/screens/homeScreen/HomeScreen.tsx
@@ -1,4 +1,23 @@
+import './homeScreen.css';
+import { homeBG } from '../../images/images.js';
+import { arrowDown, circle } from '../../icons/icons.js';
+
 function HomeScreen() {
-  return <div>HomeScreen</div>;
+  return (
+    <div className="pf__home">
+      <div className="pf__home-start">
+        <img src={homeBG} id="main-img" alt="Starting page" />
+        <div className="pf__home-start_imgOverlay" />
+        <h1>Bonjour</h1>
+        <h2>Dear Guest</h2>
+        <img
+          src={arrowDown}
+          alt="Arrow goes down"
+          className="pf__home-start_arrow"
+        />
+        <img src={circle} alt="Circle" className="pf__home-start_circle" />
+      </div>
+    </div>
+  );
 }
 export default HomeScreen;
